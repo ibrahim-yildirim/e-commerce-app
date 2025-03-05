490aa6c33a4f8e1b4d698f690c1d48fdbbc8f097
İbrahim YILDIRIM <yildirim.ibm@gmail.com>
2025-03-05 16:59:34 +0300

    feat: add basic CRUD operations without db connection

---
+++ src/main/java/com/ecommerce/site/common/Constants.java
@@ -0,0 +1,11 @@
+package com.ecommerce.site.common;
+
+public class Constants {
+
+    public static class Customer {
+
+        public static final String DELETE_SUCCESS = "Customer deleted successfully.";
+
+    }
+
+}
---
+++ src/main/java/com/ecommerce/site/controller/CustomerController.java
@@ -0,0 +1,41 @@
+package com.ecommerce.site.controller;
+
+import com.ecommerce.site.common.Constants;
+import com.ecommerce.site.entity.CustomerEntity;
+import com.ecommerce.site.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @PostMapping("create")
+    public ResponseEntity<CustomerEntity> createCustomer(@RequestBody CustomerEntity customer) {
+        return new ResponseEntity<>(customerService.createCustomer(customer), HttpStatus.CREATED);
+    }
+
+    @GetMapping("getAll")
+    public ResponseEntity<List<CustomerEntity>> getAllCustomers() {
+        return new ResponseEntity<>(customerService.getAllCustomers(), HttpStatus.OK);
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<CustomerEntity> updateCustomer(@RequestBody CustomerEntity customer) {
+        return new ResponseEntity<>(customerService.updateCustomer(customer), HttpStatus.OK);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<String> deleteCustomer(@RequestParam(value = "id") String customerId) {
+        customerService.deleteCustomer(customerId);
+        return new ResponseEntity<>(Constants.Customer.DELETE_SUCCESS, HttpStatus.OK);
+    }
+
+}
