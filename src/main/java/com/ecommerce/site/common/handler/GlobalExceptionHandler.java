f160c56129bf73e8b338aea7716853c6f005a877
İbrahim YILDIRIM <yildirim.ibm@gmail.com>
2025-03-17 17:43:31 +0300

    feat: add GlobalExceptionHandler for show IllegalArgumentException message

---
+++ src/main/java/com/ecommerce/site/common/handler/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.ecommerce.site.common.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+}
